05e301e16ba99df790a4006bfc6c57dea9fcda21
Subhash <svs.subhash2003@gmail.com>
2024-04-16 16:20:41 +0530

    connected db

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,28 @@
+const express = require('express')
+const cors = require('cors');
+const { db } = require('./db/db');
+const app = express()
+
 require('dotenv').config()
+
 
 const PORT = process.env.PORT
 
+//middlewires
+app.use(express.json())
+app.use(cors())
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
 
 const server = () => {
-    console.log('You are listening to port:', PORT)
+    db()
+    app.listen(PORT, () => {
+        console.log('listening to port :', PORT)
+    })
 }
+
 
 server()
