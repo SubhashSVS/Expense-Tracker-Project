a92c343d765a02605abffbd77089c5680a842c51
Subhash <svs.subhash2003@gmail.com>
2024-04-17 23:00:36 +0530

    Setup basic backend

--- backend/controllers/expense.js
+++ backend/controllers/expense.js
@@ -1,11 +1,11 @@
-const IncomeSchema = require("../models/incomeModel")
+const ExpenseSchema = require("../models/expenseModel")
 
 
 
-exports.addIncome = async (req, res) => {
+exports.addExpense = async (req, res) => {
     const { title, amount, category, description, date } = req.body
 
-    const income = IncomeSchema({
+    const expense = ExpenseSchema({
         title,
         amount,
         category,
@@ -20,27 +20,27 @@ exports.addIncome = async (req, res) => {
         if (amount <= 0 || !amount === 'number') {
             return res.status(400).json({ message: 'Amount must be positive' })
         }
-        await income.save()
-        res.status(200).json({ message: 'Income Added' })
+        await expense.save()
+        res.status(200).json({ message: 'Expense Added' })
     } catch (error) {
         res.status(500).json({ message: 'Server Error' })
     }
 }
 
-exports.getIncomes = async (req, res) => {
+exports.getExpenses = async (req, res) => {
     try {
-        const incomes = await IncomeSchema.find().sort({ createdAt: -1 })
-        res.status(200).json(incomes)
+        const expenses = await ExpenseSchema.find().sort({ createdAt: -1 })
+        res.status(200).json(expenses)
     } catch (error) {
         res.status(500).json({ message: 'Server Error' })
     }
 }
 
-exports.deleteIncome = async (req, res) => {
+exports.deleteExpense = async (req, res) => {
     const { id } = req.params;
-    IncomeSchema.findByIdAndDelete(id)
+    ExpenseSchema.findByIdAndDelete(id)
         .then((income) => {
-            res.status(200).json({ message: 'Income Deleted' })
+            res.status(200).json({ message: 'Expense Deleted' })
         })
         .catch((err) => {
             res.status(500).json({ message: 'Server Error' })
--- backend/routes/transactions.js
+++ backend/routes/transactions.js
@@ -1,4 +1,5 @@
-const { addIncome } = require('../controllers/income');
+const { addExpense, getExpenses, deleteExpense } = require('../controllers/expense');
+const { addIncome, getIncomes, deleteIncome } = require('../controllers/income');
 
 const router = require('express').Router()
 
@@ -6,7 +7,11 @@ const router = require('express').Router()
 
 
 router.post('/add-income', addIncome)
+    .get('/get-incomes', getIncomes)
+    .delete('/delete-income/:id', deleteIncome)
+    .post('/add-expense', addExpense)
+    .get('/get-expenses', getExpenses)
+    .delete('/delete-expense/:id', deleteExpense)
 
 
-
-module.exports = router
+module.exports = router 
