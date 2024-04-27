3927e1f4bc5f7d8ab4ed454a47952281544e26b3
Subhash <svs.subhash2003@gmail.com>
2024-04-27 13:20:16 +0530

    few changes in expense transactions

--- backend/controllers/expense.js
+++ backend/controllers/expense.js
@@ -39,10 +39,10 @@ exports.getExpenses = async (req, res) => {
 exports.deleteExpense = async (req, res) => {
     const { id } = req.params;
     ExpenseSchema.findByIdAndDelete(id)
-        .then((income) => {
+        .then((expense) => {
             res.status(200).json({ message: 'Expense Deleted' })
         })
         .catch((err) => {
             res.status(500).json({ message: 'Server Error' })
         })
-}
+} 
